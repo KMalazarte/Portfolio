132be0dbd820430940a8057f6b4aeffbb122ec3b
github username <github email address>
2019-10-01 13:20:26 -0400

    portfolio started, semantic iu react enabled

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import React, { Fragment } from 'react';
+import Nav from ./components/Nav.js
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Nav />
+    </Fragment>
+  )
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <Fragment>
+      <h1> You did it <h1>
+    </Fragment>
+  )
+}
+
+export default Nav
