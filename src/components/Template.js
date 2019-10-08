8ac0fed8ba977a158fa9f4aaab8534635a7cfd77
github username <github email address>
2019-10-08 13:15:12 -0400

    seperated mobileContainer

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,34 +1,30 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import screenShot_1 from '../images/screenshot_1.png';
 import screenShot_2 from '../images/screenshot_2.png';
 import screenShot_3 from '../images/screenshot_3.png';
-import HomepageHeading from './homepageHeading.js'
 import DesktopContainer from './desktopContainer.js'
+import MobileContainer from './mobileContainer.js'
 import {
   Button,
   Container,
   Divider,
   Grid,
   Header,
-  Icon,
   Image,
   Card,
   List,
-  Menu,
-  Responsive,
   Segment,
-  Sidebar
 } from 'semantic-ui-react'
 
 // Heads up!
 // We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
 // For more advanced usage please check Responsive docs under the "Usage" section.
-const getWidth = () => {
-  const isSSR = typeof window === 'undefined'
-
-  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
-}
+// const getWidth = () => {
+//   const isSSR = typeof window === 'undefined'
+//
+//   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
+// }
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
@@ -96,77 +92,77 @@ const getWidth = () => {
 // DesktopContainer.propTypes = {
 //   children: PropTypes.node,
 // }
-
-class MobileContainer extends Component {
-  state = {}
-
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
-
-  handleToggle = () => this.setState({ sidebarOpened: true })
-
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
-
-    return (
-      <Responsive
-        as={Sidebar.Pushable}
-        getWidth={getWidth}
-        maxWidth={Responsive.onlyMobile.maxWidth}
-      >
-        <Sidebar
-          as={Menu}
-          animation='push'
-          inverted
-          onHide={this.handleSidebarHide}
-          vertical
-          visible={sidebarOpened}
-        >
-          <Menu.Item as='a' active>
-            Home
-          </Menu.Item>
-          <Menu.Item as='a'>Work</Menu.Item>
-          <Menu.Item as='a'>Company</Menu.Item>
-          <Menu.Item as='a'>Careers</Menu.Item>
-          <Menu.Item as='a'>Log in</Menu.Item>
-          <Menu.Item as='a'>Sign Up</Menu.Item>
-        </Sidebar>
-
-        <Sidebar.Pusher dimmed={sidebarOpened}>
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 350, padding: '1em 0em' }}
-            vertical
-          >
-            <Container>
-              <Menu inverted pointing secondary size='large'>
-                <Menu.Item onClick={this.handleToggle}>
-                  <Icon name='sidebar' />
-                </Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted>
-                    Log in
-                  </Button>
-                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            </Container>
-            <HomepageHeading mobile />
-          </Segment>
-
-          {children}
-        </Sidebar.Pusher>
-      </Responsive>
-    )
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
+//
+// class MobileContainer extends Component {
+//   state = {}
+//
+//   handleSidebarHide = () => this.setState({ sidebarOpened: false })
+//
+//   handleToggle = () => this.setState({ sidebarOpened: true })
+//
+//   render() {
+//     const { children } = this.props
+//     const { sidebarOpened } = this.state
+//
+//     return (
+//       <Responsive
+//         as={Sidebar.Pushable}
+//         getWidth={getWidth}
+//         maxWidth={Responsive.onlyMobile.maxWidth}
+//       >
+//         <Sidebar
+//           as={Menu}
+//           animation='push'
+//           inverted
+//           onHide={this.handleSidebarHide}
+//           vertical
+//           visible={sidebarOpened}
+//         >
+//           <Menu.Item as='a' active>
+//             Home
+//           </Menu.Item>
+//           <Menu.Item as='a'>Work</Menu.Item>
+//           <Menu.Item as='a'>Company</Menu.Item>
+//           <Menu.Item as='a'>Careers</Menu.Item>
+//           <Menu.Item as='a'>Log in</Menu.Item>
+//           <Menu.Item as='a'>Sign Up</Menu.Item>
+//         </Sidebar>
+//
+//         <Sidebar.Pusher dimmed={sidebarOpened}>
+//           <Segment
+//             inverted
+//             textAlign='center'
+//             style={{ minHeight: 350, padding: '1em 0em' }}
+//             vertical
+//           >
+//             <Container>
+//               <Menu inverted pointing secondary size='large'>
+//                 <Menu.Item onClick={this.handleToggle}>
+//                   <Icon name='sidebar' />
+//                 </Menu.Item>
+//                 <Menu.Item position='right'>
+//                   <Button as='a' inverted>
+//                     Log in
+//                   </Button>
+//                   <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+//                     Sign Up
+//                   </Button>
+//                 </Menu.Item>
+//               </Menu>
+//             </Container>
+//             <HomepageHeading mobile />
+//           </Segment>
+//
+//           {children}
+//         </Sidebar.Pusher>
+//       </Responsive>
+//     )
+//   }
+// }
+//
+// MobileContainer.propTypes = {
+//   children: PropTypes.node,
+// }
 
 const ResponsiveContainer = ({ children }) => (
   <div>
